40172920c2789b110ad55c900794b0e134928b03
shreejadhav10 <shree@3391>
2018-05-21 18:33:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shree/navigationdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.shree.navigationdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.btnNavigationViewLaunch);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,ActDashboardNavigation.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
